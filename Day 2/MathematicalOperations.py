a2e285364e661841d094abec6e0d50c4195ec620
ibuttercup <63045112+Nandhini311@users.noreply.github.com>
2024-08-16 23:52:32 +0530

    Create MathematicalOperations.py

---
+++ Day 2/MathematicalOperations.py
@@ -0,0 +1,13 @@
+print("My age: " + str(12))
+print(7-5)
+print(7*2)
+print(8/2) #dividing always gives a floating point number -> implicit typecasting
+print(8//2) #no implicit type casting -> all the decimal points are removed.
+print(8**2) #exponents; 8^2
+
+#PEDMAS
+print(3 * 3 + 3/3-3)
+#answer: 7
+
+#change the output so it ouput is 3
+print( 3 * (3+3)//3 - 3)
