809cd3f85485f0f02239a859c24e72b3c27b7087
ibuttercup <63045112+Nandhini311@users.noreply.github.com>
2024-11-26 11:43:49 +0530

    Create HighestScore.py

---
+++ Day 4 - Python Loops /HighestScore.py
@@ -0,0 +1,20 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+print(range(1, 10))
+
+'''sum'''
+FindSum = 0
+for value in student_scores:
+    FindSum = FindSum + value
+
+print(FindSum)
+print(sum(student_scores))
+
+'''max'''
+
+maximum = 0
+for value in student_scores:
+    if maximum < value:
+        maximum = value
+
+print(maximum)
+print(max(student_scores))
